898c686eaba56ccdbae35fcfa82f670142fec85d
Alex Caza <alex@alexcaza.com>
2016-06-23 17:24:49 -0400

    First commit 💆

---
+++ app.js
@@ -0,0 +1,12 @@
+// Default packages
+import React from 'react'
+import { render } from 'react-dom'
+
+// App packages
+import AuthForm from './AuthForm/AuthForm'
+
+const app = document.getElementById("app")
+
+render(<AuthForm />, app)
+
+// export default AuthForm;
